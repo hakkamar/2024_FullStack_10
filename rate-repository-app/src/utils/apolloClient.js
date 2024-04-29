6d2fbd4977068bee8be8b0c0fffadfc332081e2a
Marko Hakkarainen <hakkis68@gmail.com>
2024-04-29 15:38:31 +0300

    10.12

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,8 +1,13 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+import Constants from "expo-constants";
+
 const createApolloClient = () => {
+  //console.log(Constants.expoConfig.extra.apolloUri);
+
   return new ApolloClient({
-    uri: "http://192.168.0.7:4000/graphql",
+    //uri: "http://192.168.0.7:4000/graphql",
+    uri: Constants.expoConfig.extra.apolloUri,
     cache: new InMemoryCache(),
   });
 };
